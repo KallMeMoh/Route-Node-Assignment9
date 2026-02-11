4e0f9fcdff0644ca094ca651c96f7d9909a1d190
KallMeMoh <justmohammad66@gmail.com>
2026-02-11 20:11:13 +0200

    Init endpoints

--- src/app.bootstrap.js
+++ src/app.bootstrap.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { PORT } from '../config/config.service.js';
 import { connectDB } from './DB/connection.js';
+import { authRouter } from './Modules/Auth/auth.controller.js';
+import { userRouter } from './Modules/User/user.controller.js';
+import { noteRouter } from './Modules/Note/note.controller.js';
 
 export default async function bootstrap() {
   const app = express();
@@ -8,6 +11,10 @@ export default async function bootstrap() {
   await connectDB();
 
   app.use(express.json());
+
+  app.use('/auth', authRouter);
+  app.use('/users', userRouter);
+  app.use('/notes', noteRouter);
 
   app.use('{/*dummy}', (req, res) => {
     return res.status(404).json({ message: 'Page not found' });
